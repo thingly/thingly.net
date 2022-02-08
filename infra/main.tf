f3f357015b778cbc26f02bbd62ed972b4d0677bb
Cliff Snyder <cliff.snyder@gmail.com>
2022-02-08 10:44:52 -0500

    vms

--- infra/main.tf
+++ infra/main.tf
@@ -65,3 +65,45 @@ resource "azurerm_network_interface" "thingly_app_nic" {
     environment = "production"
   }
 }
+
+resource "azurerm_virtual_machine" "thingly_app_vm" {
+  count               = 4
+  name                = "thingly-app-vm-${count.index}"
+  location            = azurerm_resource_group.thingly_app_rg.location
+  resource_group_name = azurerm_resource_group.thingly_app_rg.name
+  availability_set_id = azurerm_availability_set.thingly_app_aset.id
+  network_interface_ids = [
+    azurerm_network_interface.thingly_app_nic[count.index].id
+  ]
+  vm_size = "Standard_D2_v5"
+
+  storage_image_reference {
+    offer     = "0001-com-ubuntu-server-focal"
+    publisher = "Canonical"
+    sku       = "20_04-lts"
+    version   = "20.04.202201310"
+  }
+
+  storage_os_disk {
+    name          = "thingly-app-os-disk-${count.index}"
+    create_option = "FromImage"
+    caching       = "ReadWrite"
+  }
+
+  os_profile {
+    computer_name  = "thingly-app-${count.index}"
+    admin_username = "thingly"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/thingly/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa-thingly.net.pub")
+    }
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
