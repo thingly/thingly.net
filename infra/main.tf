43ab3114249c39d36e35f932fdab69103f05ff64
Cliff Snyder <cliff.snyder@gmail.com>
2022-02-07 21:40:12 -0500

    subnet

--- infra/main.tf
+++ infra/main.tf
@@ -23,13 +23,20 @@ resource "azurerm_resource_group" "thingly_app_rg" {
 }
 
 resource "azurerm_virtual_network" "thingly_app_vnet" {
-    name                = "thingly-app-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
-    tags = {
-      environment = "Production"
-    }
-    resource_group_name = azurerm_resource_group.thingly_app_rg.name
+  name          = "thingly-app-vnet"
+  address_space = ["10.0.0.0/16"]
+  location      = "westus2"
+  tags = {
+    environment = "Production"
+  }
+  resource_group_name = azurerm_resource_group.thingly_app_rg.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.thingly_app_rg.name
+  virtual_network_name = azurerm_virtual_network.thingly_app_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_availability_set" "thingly_app_aset" {
