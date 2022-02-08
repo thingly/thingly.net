25d6b208514d34611b7e76329122f863257cc856
Cliff Snyder <cliff.snyder@gmail.com>
2022-02-07 21:26:34 -0500

    availability set

--- infra/main.tf
+++ infra/main.tf
@@ -17,13 +17,27 @@ provider "azurerm" {
 resource "azurerm_resource_group" "thingly_app_rg" {
   name     = "thingly-app-rg"
   location = "westus2"
-  tags = {}
+  tags = {
+    environment = "Production"
+  }
 }
 
 resource "azurerm_virtual_network" "thingly_app_vnet" {
     name                = "thingly-app-vnet"
     address_space       = ["10.0.0.0/16"]
     location            = "westus2"
-    tags = {}
+    tags = {
+      environment = "Production"
+    }
     resource_group_name = azurerm_resource_group.thingly_app_rg.name
+}
+
+resource "azurerm_availability_set" "thingly_app_aset" {
+  name                = "thingly-app-aset"
+  location            = azurerm_resource_group.thingly_app_rg.location
+  resource_group_name = azurerm_resource_group.thingly_app_rg.name
+
+  tags = {
+    environment = "Production"
+  }
 }
