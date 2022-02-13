c786bbb05fe172b420d095c496a66346bce95624
Cliff Snyder <cliff.snyder@gmail.com>
2022-02-13 17:01:28 -0500

    trying to get app service working

--- infra/main.tf
+++ infra/main.tf
@@ -38,3 +38,22 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
+resource "azurerm_app_service" "app" {
+  name                = "thingly-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  source_control {
+    repo_url = "https://github.com/thingly/thingly.net"
+    branch   = "main"
+  }
+
+  site_config {
+    python_version = "3.4"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
