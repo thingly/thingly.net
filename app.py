81c7315387bc9f5f1dde01977e25fef2293177c3
Cliff Snyder <cliff.snyder@gmail.com>
2022-02-14 19:32:52 -0500

    basic dice api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, jsonify, render_template
+from thingly import dice
 
 app = Flask(__name__)
 
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/dice')
+@app.route('/api/dice/<int:n>')
+@app.route('/api/dice/<int:n>/<int:d>')
+def roll_dice(n=1, d=6):
+    # TODO protect against overly-large n, d
+    return jsonify(dice.roll(n, d))
---
+++ thingly/dice.py
@@ -0,0 +1,4 @@
+import random
+
+def roll(n=1, d=6):
+    return [random.randint(1, d) for i in range(0, n)]
